3bcbfd82b5e29f4f442064a496800d165edc69a1
Monisha Khapali <monisha.khapali@logicsolutions.com>
2020-10-28 21:42:00 -0400

    added

---
+++ Removeoutterbrackets/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+
+namespace Removeoutterbrackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            string s = "(()())";
+            string r = RemoveOuterParentheses(s);
+
+        }
+        static string RemoveOuterParentheses(string S)
+        {
+            StringBuilder st = new StringBuilder();
+            int count = 0;
+
+            for (int i = 0; i < S.Length; i++)
+            {
+                if (S[i] == '(')
+                {
+                    if (count != 0) { st.Append(S[i]); }
+                    count++;
+                }
+                else
+                {
+                    count--;
+                    if (count != 0) { st.Append(S[i]); }
+                }
+            }
+
+            return st.ToString();
+        }
+    }
+}
